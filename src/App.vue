1bcbb6420cd653744aa934bae905898ed8f8516e
Gerwin Overeem <gerwin@overeem.io>
2019-12-12 22:21:26 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,392 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--Preloader div-->
+    <div class="preloader"></div>
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="#top">Start</a></li>
+                    <li><a href="#schedule">Programma</a></li>
+                    <li><a href="#sponsors">Sponsors</a></li>
+                    <li><a href="#venue">Locatie</a></li>
+                    <li><a href="#contact">Aanmelden</a></li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </nav>
+    <!-- // End Fixed navbar -->
+
+    <header id="top" class="header">
+        <div class="container header-container">
+
+            <div class="header_top-bg">
+                <div class="logo">
+                    
+                </div>
+            </div>
+
+            <h3 class="headline-support wow fadeInDown">AFSCHEID MARIA SCHUTTE</h3>
+            <p class="wow fadeInDown">Stafarts Centrum Jeugd en Gezin Apeldoorn</p>
+
+            <h1 class="headline wow fadeInDown" data-wow-delay="0.1s">HET KIND EERST</h1>
+            <p class="wow fadeInDown">kinderrecht | art. 03</p>
+
+            <div class="when_where wow fadeIn" data-wow-delay="0.4s">
+                <p class="event_when">24 januari 2020</p>
+                <p class="event_where">Veluwse Schavuyt, Apeldoorn</p>
+            </div>
+
+            <div class="header_bottom-bg">
+                <a class="btn btn-default btn-xl wow zoomIn" data-wow-delay="0.3s" href="#contact">AANMELDEN</a>
+            </div>
+
+        </div>
+        <!-- end .container -->
+    </header>
+    <!-- end .header -->
+
+    <!-- 
+     Highlight Section
+     ====================================== -->
+
+    <section class="highlight">
+
+        <div class="container">
+            <p class="lead text-center">Gather is a responsive website template for events, conferences, meetups, webinars etc. This template can be used of all type of gatherings. It has many features specifically designed for events like RSVP, Eventbrite Integration, Subscribe and contact. it also comes with different color versions to choose.</p>
+
+            <div class="countdown_wrap">
+                <!-- Countdown JS for the Event Date Starts here.
+    TIP: You can change your event time below in the Same Format.  -->
+
+                <ul id="countdown" data-event-date="24 january 2020 16:00:00">
+                    <li class="wow zoomIn" data-wow-delay="0s"> <span class="days">00</span>
+                        <p class="timeRefDays">days</p>
+                    </li>
+                    <li class="wow zoomIn" data-wow-delay="0.2s"> <span class="hours">00</span>
+                        <p class="timeRefHours">hours</p>
+                    </li>
+                    <li class="wow zoomIn" data-wow-delay="0.4s"> <span class="minutes">00</span>
+                        <p class="timeRefMinutes">minutes</p>
+                    </li>
+                    <li class="wow zoomIn" data-wow-delay="0.6s"> <span class="seconds">00</span>
+                        <p class="timeRefSeconds">seconds</p>
+                    </li>
+                </ul>
+            </div>
+            <!-- end .countdown_wrap -->
+
+            <div class="text-center">
+
+                <!-- Add to Calendar Plugin. 
+                     For Customization, Visit https://addtocalendar.com/ -->
+
+                <span class="addtocalendar atc-style-theme">
+                <a class="atcb-link"><i class="fa fa-calendar"> </i> Voeg toe aan uw agenda</a>
+                  <var class="atc_event">
+                      <var class="atc_date_start">2020-01-24 16:00:00</var>
+                      <var class="atc_date_end">2016-01-24 20:00:00</var>
+                      <var class="atc_timezone">Europe/Amsterdam</var>
+                      <var class="atc_title">Afscheid Maria Schutte</var>
+                      <var class="atc_description"></var>
+                      <var class="atc_location">Veluwse Schavuyt, Apeldoorn</var>
+                  </var>
+               </span>
+
+            </div>
+
+        </div>
+        <!-- end .container -->
+
+    </section>
+    <!-- end section.highlight -->
+
+    <!-- 
+     Our Schedule
+     ====================================== -->
+
+    <section class="schedule" id="schedule">
+
+        <div class="container">
+            <div class="section-title wow fadeInUp">
+                <h4>PROGRAMMA</h4>
+            </div>
+
+            <!-- Tab panes -->
+            <div class="tab-content">
+                <div role="tabpanel" class="tab-pane active" id="day1">
+
+                    <!-- ########## Schedule Timeline DAY 01 Starts ########### -->
+
+                    <section class="timeline">
+                        <div class="timeline-block">
+                            <div class="timeline-bullet wow zoomIn" data-wow-delay="0s">
+
+                            </div>
+                            <!-- timeline-bullet -->
+
+                            <div class="timeline-content">
+                                <h2 class="wow flipInX" data-wow-delay="0.3s"> Ontvangst met koffie</h2>
+                                <span class="date wow flipInX" data-wow-delay="0.3s">16:00</span>
+                            </div>
+                            <!-- timeline-content -->
+                        </div>
+                        <!-- timeline-block -->
+
+                        <div class="timeline-block">
+                            <div class="timeline-bullet wow zoomIn" data-wow-delay="0s">
+
+                            </div>
+                            <!-- timeline-bullet -->
+
+                            <div class="timeline-content">
+                                <div class="speaker-info wow fadeIn" data-wow-delay="0s">
+                                    <img src="images/willemijn.jpg" alt="avatar" class="img-responsive">
+                                </div>
+                                <h2 class="wow flipInX" data-wow-delay="0.3s"> Willemijn Dupuis</h2>
+                                <p class="wow flipInX" data-wow-delay="0.3s">Willemijn Dupuis heeft vanaf 1987 met Kinderrechtenambassadeurs gewerkt. Zij is begonnen als vrijwilliger voor UNICEF en heeft met Kinderrechtenambassadeurs van alle Westlandse basisscholen kinderparticipatie op de kaart gezet. Met de kinderen werd gesproken over hun rechten en plichten en die van alle kinderen in de wereld.</p>
+                                <span class="date wow flipInX" data-wow-delay="0.3s">16:30</span>
+                            </div>
+                            <!-- timeline-content -->
+                        </div>
+                        <!-- timeline-block -->
+
+                        <div class="timeline-block">
+                            <div class="timeline-bullet wow zoomIn" data-wow-delay="0s">
+
+                            </div>
+                            <!-- timeline-bullet -->
+
+                            <div class="timeline-content end">
+                                <div class="speaker-info wow fadeIn justify-content-end" data-wow-delay="0s">
+                                    <img src="images/willemijn.jpg" alt="avatar" class="img-responsive">
+                                </div>
+                                <h2 class="wow flipInX" data-wow-delay="0.3s"> Willemijn Dupuis</h2>
+                                <p class="wow flipInX" data-wow-delay="0.3s">Willemijn Dupuis heeft vanaf 1987 met Kinderrechtenambassadeurs gewerkt. Zij is begonnen als vrijwilliger voor UNICEF en heeft met Kinderrechtenambassadeurs van alle Westlandse basisscholen kinderparticipatie op de kaart gezet. Met de kinderen werd gesproken over hun rechten en plichten en die van alle kinderen in de wereld.</p>
+                                <span class="date wow flipInX" data-wow-delay="0.3s">17:00</span>
+                            </div>
+                            <!-- timeline-content -->
+                        </div>
+                        <!-- timeline-block -->
+
+                        <div class="timeline-block">
+                            <div class="timeline-bullet wow zoomIn" data-wow-delay="0s">
+
+                            </div>
+                            <!-- timeline-bullet -->
+
+                            <div class="timeline-content">
+                                <h2 class="wow flipInX" data-wow-delay="0.3s"> Toost & Toespraken</h2>
+                                <span class="date wow flipInX" data-wow-delay="0.3s">17:30</span>
+                            </div>
+                            <!-- timeline-content -->
+                        </div>
+                        <!-- timeline-block -->
+
+                        <div class="timeline-block">
+                            <div class="timeline-bullet wow zoomIn" data-wow-delay="0s">
+
+                            </div>
+                            <!-- timeline-bullet -->
+
+                            <div class="timeline-content">
+                                <h2 class="wow flipInX" data-wow-delay="0.3s"> Rondleiding door de brouwerij</h2>
+                                <p class="wow flipInX end" data-wow-delay="0.3s">(optioneel)</p>
+                                <span class="date wow flipInX" data-wow-delay="0.3s">18:00</span>
+                            </div>
+                            <!-- timeline-content -->
+                        </div>
+                        <!-- timeline-block -->
+
+                        <div class="timeline-block">
+                            <div class="timeline-bullet wow zoomIn" data-wow-delay="0s">
+
+                            </div>
+                            <!-- timeline-bullet -->
+
+                            <div class="timeline-content">
+                                <h2 class="wow flipInX" data-wow-delay="0.3s"> Bier & Bitterballen</h2>
+                                <span class="date wow flipInX" data-wow-delay="0.3s">18:00</span>
+                            </div>
+                            <!-- timeline-content -->
+                        </div>
+                        <!-- timeline-block -->
+
+                        <div class="timeline-block">
+                            <div class="timeline-bullet wow zoomIn" data-wow-delay="0s">
+
+                            </div>
+                            <!-- timeline-bullet -->
+
+                            <div class="timeline-content">
+                                <h2 class="wow flipInX" data-wow-delay="0.3s"> Einde</h2>
+                                <span class="date wow flipInX" data-wow-delay="0.3s">20:00</span>
+                            </div>
+                            <!-- timeline-content -->
+                        </div>
+                        <!-- timeline-block -->
+                    </section>
+                    <!-- timeline -->
+
+                    <!-- ########### End Schedule Timeline  DAY 01  ########### -->
+
+                </div>
+            </div>
+
+        </div>
+        <!-- end .container -->
+
+    </section>
+    <!-- end section.schedule -->
+
+    <!-- 
+     Our Sponsors
+     ====================================== -->
+
+    <section class="sponsors" id="sponsors">
+
+        <div class="container">
+            <div class="section-title wow fadeInUp">
+                <h4>SPONSORS</h4>
+                <p>Mede mogelijk gemaakt door:</p>
+            </div>
+
+            <div class="sponsor-slider wow bounceIn">
+                <div><img src="images/sponsor_1.png" class="img-responsive center-block" alt="sponsor"> </div>
+                <div><img src="images/sponsor_2.png" class="img-responsive center-block" alt="sponsor"> </div>
+                <div><img src="images/sponsor_3.png" class="img-responsive center-block" alt="sponsor"> </div>
+                <div><img src="images/sponsor_4.png" class="img-responsive center-block" alt="sponsor"> </div>
+                <div><img src="images/sponsor_1.png" class="img-responsive center-block" alt="sponsor"> </div>
+                <div><img src="images/sponsor_2.png" class="img-responsive center-block" alt="sponsor"> </div>
+                <div><img src="images/sponsor_3.png" class="img-responsive center-block" alt="sponsor"> </div>
+                <div><img src="images/sponsor_4.png" class="img-responsive center-block" alt="sponsor"> </div>
+            </div>
+        </div>
+        <!-- end .container -->
+    </section>
+    <!-- end section.sponsors -->
+
+    <!-- 
+     Contact us
+     ====================================== -->
+
+    <div class="highlight highlight-venue" id="venue">
+        <div class="container highlight-venue-container">
+
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="contact-box">
+                        <img src="images/location-icon.png" alt="location icon" class="wow zoomIn">
+                        <h5>ADRES</h5>
+                        <p>Bierbrouwerij de Vlijt
+                            <br>Vlijtseweg 114
+                            <br>7317 AG Apeldoorn
+                            <br>
+                            <br>
+                            <a href="https://www.veluwseschavuyt.nl/">Meer over de Veluwse Schavuyt</a>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="contact-box">
+                        <img src="images/email-icon.png" alt="email icon" class="wow zoomIn" data-wow-delay="0.3s">
+                        <h5>BEREIKBAARHEID</h5>
+                        <p>Parkeren bij de locatie is gratis.
+                            <br> Er zijn voldoende parkeerplekken beschikbaar.
+                            <br>
+                            <br> Er gaat ook nog een bus.
+                            <br> 
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!--  // end .row -->
+        </div>
+    </div>
+    <!-- //  end .highlight -->
+
+    <div class="container" id="contact">
+        <div class="section-title">
+            <h4>AANMELDEN</h4>
+            <p>Vul onderstaand formulier in om u aan te melden voor het afscheid.</p>
+        </div>
+
+        <div class="contact-form bottom-space-xl wow fadeInUp">
+
+            <form action="php/contact.php" id="phpcontactform" method="POST">
+                <div class="row">
+
+                    <div class="col-md-6 col-md-offset-3">
+
+                        <div class="row">
+
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Voornaam *</label>
+                                    <input type="text" class="form-control" name="first_name" placeholder="Voornaam" required>
+
+                                </div>
+
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Achternaam *</label>
+                                    <input type="text" class="form-control" name="last_name" placeholder="Achternaam" required>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label>E-mailadres *</label>
+                            <input type="email" class="form-control" name="email" placeholder="E-mailadres" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Jouw boodschap aan Maria (optioneel)</label>
+                            <textarea class="form-control" name="message" rows="6"> </textarea>
+                        </div>
+
+                        <div class="text-center top-space">
+                            <button type="submit" class="btn btn-success btn-block btn-lg" id="js-contact-btn">Verstuur</button>
+                            <div id="js-contact-result" data-success-msg="Form submitted successfully." data-error-msg="Oops. Something went wrong."></div>
+                        </div>
+
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </div>
+    <!-- // end .container -->
+
+    <footer>
+        <p> <small class="text-muted">Copyright © 2019. All rights reserved. Made in Amsterdam.</small></p>
+
+    </footer>
+
+    <a href="#top" class="back_to_top"><img src="images/back_to_top.png" alt="back to top"></a>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
